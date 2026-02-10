dffb9fd3eff0da8465106959d597d3a5bfca40a1
Grozamir <48309973+Grozamir@users.noreply.github.com>
2026-02-10 16:52:27 +0700

    It's alive!

---
+++ library.cpp
@@ -0,0 +1,36 @@
+#include "library.h"
+
+namespace mathlib {
+
+    int32_t add(int32_t a, int32_t b) {
+        return a + b;
+    }
+
+    int32_t sub(int32_t a, int32_t b) {
+        return a - b;
+    }
+
+    int32_t mul(int32_t a, int32_t b) {
+        return a * b;
+    }
+
+    int32_t div(int32_t a, int32_t b) {
+        return a / b;
+    }
+
+    int32_t pow(int32_t a, int32_t b) {
+        int32_t result = a;
+        for ( int32_t i = 0; i < b; ++i ) {
+            result *= a;
+        }
+        return 0;
+    }
+
+    int32_t factorial(int32_t a) {
+        if ( a == 1 ) {
+            return 1;
+        }
+        return a * factorial(a - 1);
+    }
+
+}
---
+++ library.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace mathlib {
+    int32_t add(int32_t a, int32_t b);
+    int32_t sub(int32_t a, int32_t b);
+    int32_t mul(int32_t a, int32_t b);
+    int32_t div(int32_t a, int32_t b);
+    int32_t pow(int32_t a, int32_t b);
+    int32_t factorial(int32_t a);
+}
